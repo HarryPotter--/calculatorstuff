1c9ed3f6a842ce61cbc16d07a584c49f326d6e32
HarryPotter- <bm49731@westport.k12.ct.us>
2014-09-11 08:34:36 -0400

    Initial Commit

---
+++ calculator1.rb
@@ -0,0 +1,13 @@
+puts "Why hello there! i am a area of a square area finding calculator!"
+i = gets
+puts "what is your items length?"
+length = gets.to_i
+puts "what is your items width?"
+width = gets.to_i
+puts "what is your items hight?"
+hight = gets.to_i
+puts "your area is " + (length * width * hight).to_s + " Be sure to thank your captian. Johnny mcJohnPhillipson"
+thanks = gets
+puts " your mom made me do your laundry yesterday. I will never see the world as i used to."
+
+
